08de8464d85b7bc80ebfbd5edd4b79d064817dce
Stanislav Lakhtin <lakhtin.stanislav@gmail.com>
2025-11-11 12:09:40 +0300

    colors experiments

--- calibrationApp/src/commonMain/kotlin/me/lakhtin/themecalibration/ui/screens/main/MainScreen.kt
+++ calibrationApp/src/commonMain/kotlin/me/lakhtin/themecalibration/ui/screens/main/MainScreen.kt
@@ -6,8 +6,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,14 +18,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import me.lakhtin.themecalibration.Greeting
 import me.lakhtin.themecalibration.Route
+import me.lakhtin.themecalibration.ui.theme.ActionButton
+import me.lakhtin.themecalibration.ui.theme.gradientButtonColors
+import me.lakhtin.themecalibration.ui.theme.primaryButtonColors
 import org.jetbrains.compose.resources.painterResource
 import themecalibration.calibrationapp.generated.resources.Res
 import themecalibration.calibrationapp.generated.resources.compose_multiplatform
 
 @Composable
-fun MainScreen (
+fun MainScreen(
     navigateTo: (Route) -> Unit
 ) {
     MainScreenView(
@@ -42,8 +49,14 @@ fun MainScreenView(navigateTo: (Route) -> Unit) {
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Button(onClick = { showContent = !showContent }) {
-            Text("Click me!")
+        ActionButton(
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp)
+                .height(48.dp),
+            onClick = { showContent = !showContent },
+            colors = gradientButtonColors()
+        ) {
+            Text("Click me!", style = MaterialTheme.typography.titleLarge)
         }
         AnimatedVisibility(showContent) {
             val greeting = remember { Greeting().greet() }
@@ -55,8 +68,21 @@ fun MainScreenView(navigateTo: (Route) -> Unit) {
                 Text("Compose: $greeting")
             }
         }
-        Button(onClick = { navigateTo(Route.TypographyScreenRoute) }) {
-            Text("Typography screen")
+        ActionButton(
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp)
+                .height(48.dp),
+            onClick = { navigateTo(Route.TypographyScreenRoute) },
+            colors = primaryButtonColors()
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp),
+                text = "Typography screen",
+                maxLines = 1,
+                overflow = TextOverflow.MiddleEllipsis,
+                style = MaterialTheme.typography.displaySmall
+            )
         }
     }
 }
--- calibrationApp/src/commonMain/kotlin/me/lakhtin/themecalibration/ui/theme/DefaultColors.kt
+++ calibrationApp/src/commonMain/kotlin/me/lakhtin/themecalibration/ui/theme/DefaultColors.kt
@@ -4,18 +4,58 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun primaryActionButtonColors() = SimpleActionButtonColors(
-    containerColor = MaterialTheme.colorScheme.primary,
-    contentColor = MaterialTheme.colorScheme.onPrimary,
-    borderColor = MaterialTheme.colorScheme.outline,
+fun actionButtonColors(
+    containerColor: Color = MaterialTheme.colorScheme.primary,
+    contentColor: Color = MaterialTheme.colorScheme.onPrimary,
+    borderColor: Color = Color.Unspecified,
+    borderWidth: Dp = 0.dp
+) = SimpleActionButtonColors(
+    containerColor = containerColor,
+    contentColor = contentColor,
+    borderColor = borderColor,
+    borderWidth = borderWidth
+)
+
+@Composable
+fun primaryButtonColors(
+    containerColor: Color = MaterialTheme.colorScheme.primary,
+    contentColor: Color = MaterialTheme.colorScheme.onPrimary,
+    borderColor: Color = Color.Unspecified,
+    borderWidth: Dp = 0.dp
+) = actionButtonColors(
+        containerColor = containerColor,
+        contentColor = contentColor,
+        borderColor = borderColor,
+        borderWidth = borderWidth
+    )
+
+@Composable
+fun secondaryButtonColors() = actionButtonColors(
+        containerColor = MaterialTheme.colorScheme.secondary,
+        contentColor = MaterialTheme.colorScheme.onSecondary,
+        borderColor = Color.Unspecified,
+        borderWidth = 0.dp
+    )
+
+@Composable
+fun errorButtonColors() = actionButtonColors(
+    containerColor = MaterialTheme.colorScheme.error,
+    contentColor = MaterialTheme.colorScheme.onError,
+    borderColor = Color.Unspecified,
     borderWidth = 0.dp
 )
 
 @Composable
-fun primaryGradientButtonColors() = GradientActionButtonColors(
+fun gradientButtonColors(
+    containerBrush: Color = MaterialTheme.colorScheme.primary,
+    contentColor: Color = MaterialTheme.colorScheme.onPrimary,
+    borderColor: Color = Color.Unspecified,
+    borderWidth: Dp = 0.dp
+) = GradientActionButtonColors(
     containerBrush = createGradientBrush(
         gradientType = GradientType.LINEAR,
         gradientDirection = GradientDirection.LEFT_TO_RIGHT,
@@ -24,15 +64,7 @@ fun primaryGradientButtonColors() = GradientActionButtonColors(
             MaterialTheme.colorScheme.secondary
         )
     ),
-    contentColor = MaterialTheme.colorScheme.onPrimary,
-    borderColor = Color.Unspecified,
-    borderWidth = 0.dp,
-)
-
-@Composable
-fun grayCardColors() = CardDefaults.cardColors(
-    containerColor = MaterialTheme.colorScheme.secondaryContainer,
-    contentColor = MaterialTheme.colorScheme.onSecondaryContainer,
-    disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-    disabledContentColor = MaterialTheme.colorScheme.outline
+    contentColor = contentColor,
+    borderColor = borderColor,
+    borderWidth = borderWidth,
 )
